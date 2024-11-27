b27b8ed1bdcab6793259d2dd0b5cc43ad3c4c402
kan-pawes <kan-pawes@fiskeridir.no>
2024-11-27 13:10:44 +0100

    add simple playwright-axe test

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Example project for a11y testing
+# Example project for a11y testing with Playwright
+
+Goal is to show how simple it can be to set up accessibility testing for any web page.
+With just two targeted tests, we aim to address many of the common a11y pitfalls in web app development also 
+ensuring that developers prioritize accessibility in every stage of the development process.
 
 ## Aria snapshots
 
@@ -8,7 +12,11 @@ To update all snapshot use `npx playwright test --update-snapshots`
 
 Read more about aria in [docs](https://playwright.dev/docs/aria-snapshots)
 
-## Prosjektet 
+## A11y testing
+
+Run simple Axe analyze to discover any violations
+
+## Other 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,33 +1,40 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
+const AxeBuilder = require('@axe-core/playwright').default;
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await expect(page.locator('#root')).toMatchAriaSnapshot(`
-    - heading "Eventyrbilder" [level=4]
-    - textbox "Søk"
-    - img "Bilde"
-    - text: Tid
-    - img "Bilde"
-    - text: Noe glemt
-    - img "Bilde"
-    - text: Et forsøk
-    - img "Bilde"
-    - text: Uheldig
-    - img "Bilde"
-    - text: Fri
-    - img "Bilde"
-    - text: Nytt liv
-    - img "Bilde"
-    - text: Evner
-    - img "Bilde"
-    - text: Fly
-    - img "Bilde"
-    - text: Det første som faller
+test.describe('homepage', () => { // 2
+    test('has title', async ({page}) => {
+        await page.goto('http://localhost:3000/');
+        await expect(page.locator('#root')).toMatchAriaSnapshot(`
+        - heading "Eventyrbilder" [level=4]
+        - textbox "Søk"
+        - img "Bilde"
+        - text: Tid
+        - img "Bilde"
+        - text: Noe glemt
+        - img "Bilde"
+        - text: Et forsøk
+        - img "Bilde"
+        - text: Uheldig
+        - img "Bilde"
+        - text: Fri
+        - img "Bilde"
+        - text: Nytt liv
+        - img "Bilde"
+        - text: Evner
+        - img "Bilde"
+        - text: Fly
+        - img "Bilde"
+        - text: Det første som faller
     `);
-});
+    });
 
-// test('get started link', async ({ page }) => {
-//
-//
-// });
+
+    test('should not have any automatically detectable accessibility issues', async ({page}) => {
+        await page.goto('http://localhost:3000/');
+
+        const accessibilityScanResults = await new AxeBuilder({page}).analyze();
+
+        expect(accessibilityScanResults.violations).toEqual([]);
+    });
+});
